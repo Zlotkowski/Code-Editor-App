c2355f6f56036cbee33f4b1e50f97849552f8006
Zlotkowski <zlotkowskimariusz@gmail.com>
2021-08-18 11:28:58 +0200

    Creating Routes component, find some bugs with, file name's => comment in AppRoutes.tsx

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { Redirect, Route, Switch } from "react-router-dom";
+import ProtectedRoute from "../auth/ProtectedRoute";
+import routes from "./routes";
+import { useAuth0 } from "@auth0/auth0-react";
+import Loading from "../components/common/Loading/Loading";
+
+// bug => ./src/App.tsx
+// Cannot find file: 'AppRoutes.ts' does not match the corresponding name on disk: '.\src\routes\routes.ts'.
+// When have routes.ts and AppRoutes.jsx and call to AppRoutes component from App.tsx in one folder...change name to AppRoutes.tsx
+
+const AppRoutes = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+  const classes = useStyles();
+  const example = () => <div>Example</div>;
+  if (isLoading) {
+    return <Loading />;
+  }
+  return (
+    <div className={classes.main}>
+      <div>Header</div>
+      <div className={classes.page}>
+        <Switch>
+          <ProtectedRoute exact path={routes.codeEditor} component={example} />
+          <Route exact path={routes.home}>
+            {isAuthenticated ? (
+              <Redirect to={routes.codeEditor} />
+            ) : (
+              <div>Home</div>
+            )}
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+const useStyles = makeStyles(() => ({
+  main: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  page: {
+    height: "100%",
+  },
+}));
+
+export default AppRoutes;
